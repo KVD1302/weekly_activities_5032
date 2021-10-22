fac56c09bca953e35cfc4688d415a5e11be5fab4
Vinaya Datta Kavuluri <84089064+KVD1302@users.noreply.github.com>
2021-10-22 17:09:07 +0530

    week 3 submission

---
+++ Week 3/lab3_snippet/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace lab3_snippet.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            //ViewBag.Message = "Your application description page.";
+            Models.HelloWorld.Hello hello = new Models.HelloWorld.Hello();
+            ViewBag.Message = hello.GetHello();
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
